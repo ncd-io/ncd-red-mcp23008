42015da3093c0235b86f9982d79137da4393deb2
Trey Felton <trey@controleverything.com>
2018-03-22 15:01:40 -0500

    Move class to index.js for use outside of node-red

--- ncd-mcp23008.js
+++ ncd-mcp23008.js
@@ -1,22 +1,6 @@
 "use strict";
 
-function ensureDependencies(deps){
-	//deps is just an array of dependencies that should be NPM installable, if no version is
-	//required, and array of string may be sent, if a version is needed, an array of objects:
-	//{name: "module-name", version: "^3.0.0"}
-	var registry = require(require.main.filename.replace('red.js', 'red/runtime/nodes/registry/index.js'));
-	var modules = registry.getModuleList();
-	deps.forEach((mod) => {
-		var o = typeof mod == "object";
-		var m = o ? mod.name : mod;
-		var v = o ? mod.verison : false;
-		if(typeof modules[m] == 'undefined'){
-			registry.installModule(m, v).then().catch((err) => {
-				console.log("ensureDependencies Failed to install "+m+" from "+module.filename);
-			});
-		}
-	});
-}
+const MCP23008 = require("./index.js");
 
 module.exports = function(RED){
 	var sensor_pool = {};
@@ -117,91 +101,4 @@ module.exports = function(RED){
 		});
 	}
 	RED.nodes.registerType("ncd-mcp23008", NcdI2cDeviceNode)
-}
-
-class MCP23008{
-	constructor(addr, config, comm){
-		this.data = config;
-		this.comm = comm;
-		this.addr = addr;
-		this.settable = [];
-		this.initialized = true;
-		try{
-			this.init();
-		}catch(e){
-			console.log({'failed to initialize': e});
-			this.initialized = false;
-		}
-	}
-
-	init(){
-		this.iodir = 0;
-		this.data.ios = {};
-		for(var i=8;i>0;i--){
-			this.iodir = (this.iodir << 1) | (this.data["io_"+i] ? 0 : 1);
-			this.data.ios[i] = this.data["io_"+i] ? 1 : 0;
-		}
-		Promise.all([
-			this.comm.writeBytes(this.addr, 0x00, this.iodir),
-			this.comm.writeBytes(this.addr, 0x06, this.iodir)
-		]).then().catch();
-		this.settable = ['all', 'channel_1', 'channel_2', 'channel_3', 'channel_4', 'channel_5', 'channel_6', 'channel_7', 'channel_8'];
-	}
-	get(){
-		var sensor = this;
-		return new Promise((fulfill, reject) => {
-			Promise.all([
-				sensor.comm.readByte(sensor.addr, 9),
-				sensor.comm.readByte(sensor.addr, 10)
-			]).then((res) => {
-				sensor.input_status = res[0];
-				sensor.output_status = res[1];
-				var readings = sensor.parseStatus();
-				fulfill(readings);
-			}).catch();
-		});
-	}
-	parseStatus(){
-		var ios = this.data.ios,
-			readings = {};
-		for(var i in ios){
-			if(ios[i] == 1) readings["channel_"+i] = this.output_status & (1 << (i-1)) ? 1 : 0;
-			else readings["channel_"+i] = this.input_status & (1 << (i-1)) ? 0 : 1;
-		}
-		return readings;
-	}
-	set(topic, value){
-		var sensor = this;
-		return new Promise((fulfill, reject) => {
-			//sensor.get().then((res) => {
-				var status = sensor.output_status;
-				if(topic == 'all'){
-					if(status != value){
-						sensor.output_status = value;
-						sensor.comm.writeBytes(this.addr, 0x0A, value).then(fulfill(sensor.parseStatus())).catch(reject);
-					}else{
-						fulfill(res);
-					}
-				}else{
-					var channel = topic.split('_')[1];
-					if(value == 1){
-						status |= (1 << (channel-1));
-					}else if(value == 2){
-						status ^= (1 << (channel-1));
-					}else{
-						status &= ~(1 << (channel - 1));
-					}
-					if(sensor.output_status != status){
-						sensor.output_status = status;
-						sensor.comm.writeBytes(sensor.addr, 0x0A, status).then(fulfill(sensor.parseStatus())).catch(reject);
-					}else{
-						fulfill(sensor.parseStatus());
-					}
-				}
-			// }).catch((err) => {
-			// 	console.log(err);
-			// 	reject(err);
-			// });
-		});
-	}
 }
